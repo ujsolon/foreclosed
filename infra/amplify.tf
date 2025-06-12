2faf39c12ca6d58e922716305c3aebb54c02133b
Ulyses Solon <ulyses.a.solon.jr@accenture.com>
2025-06-13 00:55:33 +0800

    Create amplify resource and init pipeline

---
+++ infra/amplify.tf
@@ -0,0 +1,28 @@
+resource "aws_amplify_app" "foreclosed_frontend" {
+  name       = "foreclosed_frontend"
+  repository = "https://github.com/ujsolon/foreclosed"
+  oauth_token = var.github_token
+
+  # Point to where the amplify.yml file is LOCALLY during terraform apply
+  build_spec = file("../frontend/amplify.yml")  # if it's one dir above infra/
+
+  environment_variables = {
+    ENVIRONMENT = "dev"
+  }
+
+  platform = "WEB_COMPUTE"
+}
+
+
+resource "aws_amplify_branch" "foreclosed_frontend_main" {
+  app_id      = aws_amplify_app.foreclosed_frontend.id
+  branch_name = "main"  # or whatever branch you want to auto-deploy
+
+  stage = "DEVELOPMENT"
+
+  environment_variables = {
+    NODE_ENV = "development"
+  }
+
+  enable_auto_build = true
+}
---
+++ infra/variables.tf
@@ -0,0 +1,5 @@
+variable "github_token" {
+  description = "GitHub OAuth token for Amplify"
+  type        = string
+  sensitive   = true
+}
