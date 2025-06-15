e922e9e211c5b051f6f364cf0579a79a6d063264
Ulyses Solon <ulyses.a.solon.jr@accenture.com>
2025-06-15 16:55:24 +0800

    Create s3 bucket for storing raw data, create iam policy for accessing bucket and role policy to give to lambda functions

--- infra/s3.tf
+++ infra/s3.tf
@@ -1,5 +1,6 @@
+# S3 Bucket for hosting a static website
 resource "aws_s3_bucket" "website" {
-  bucket = "my-foreclosed-site"  # must be globally unique
+  bucket = "my-foreclosed-site"
 
   tags = {
     Environment = "dev"
@@ -49,4 +50,41 @@ resource "aws_s3_bucket_public_access_block" "block" {
   block_public_policy     = false
   ignore_public_acls      = false
   restrict_public_buckets = false
+}
+
+# S3 Bucket for storing raw data
+resource "aws_s3_bucket" "raw_data" {
+  bucket = "raw-data"
+
+  tags = {
+    Environment = "dev"
+    Project     = "foreclosed"
+  }
+}
+
+resource "aws_iam_policy" "lambda_s3_access_policy" {
+  name = "lambda_s3_access_policy"
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement: [
+      {
+        Effect = "Allow",
+        Action = [
+          "s3:PutObject",
+          "s3:GetObject",
+          "s3:ListBucket"
+        ],
+        Resource = [
+          aws_s3_bucket.raw_data.arn,
+          "${aws_s3_bucket.intermediate_results.arn}/*"
+        ]
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "attach_s3_access_policy" {
+  role       = aws_iam_role.lambda_exec_role.name
+  policy_arn = aws_iam_policy.lambda_s3_access_policy.arn
 }
