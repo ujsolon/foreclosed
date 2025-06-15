6a152ba4ed9c784a49d70c8fe6281cb29976a1b5
Ulyses Solon <ulyses.a.solon.jr@accenture.com>
2025-06-15 20:20:23 +0800

    Create scraper_lambda via tf replacing batch_lambda

--- infra/lambda.tf
+++ infra/lambda.tf
@@ -19,13 +19,13 @@ resource "aws_iam_role_policy_attachment" "lambda_basic_execution" {
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
 
-resource "aws_lambda_function" "batch_lambda" {
-  function_name = "BatchLambda"
-  filename      = "${path.module}/batch_lambda.zip"
+resource "aws_lambda_function" "scrape_lambda" {
+  function_name = "ScrapeLambda"
+  filename      = "${path.module}/scrape_lambda.zip"
   handler       = "lambda_function.lambda_handler"
   runtime       = "python3.11"
 
-  source_code_hash = filebase64sha256("${path.module}/batch_lambda.zip")
+  source_code_hash = filebase64sha256("${path.module}/scrape_lambda.zip")
   role             = aws_iam_role.lambda_exec_role.arn
 
   environment {
