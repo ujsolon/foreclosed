b148fb86d2f2627b545fade9021952017ace4173
Ulyses Solon <ulyses.a.solon.jr@accenture.com>
2025-06-15 20:23:12 +0800

    Update scrape to scraper

--- infra/lambda.tf
+++ infra/lambda.tf
@@ -19,13 +19,13 @@ resource "aws_iam_role_policy_attachment" "lambda_basic_execution" {
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
 
-resource "aws_lambda_function" "scrape_lambda" {
-  function_name = "ScrapeLambda"
-  filename      = "${path.module}/scrape_lambda.zip"
+resource "aws_lambda_function" "scraper_lambda" {
+  function_name = "ScraperLambda"
+  filename      = "${path.module}/scraper_lambda.zip"
   handler       = "lambda_function.lambda_handler"
   runtime       = "python3.11"
 
-  source_code_hash = filebase64sha256("${path.module}/scrape_lambda.zip")
+  source_code_hash = filebase64sha256("${path.module}/scraper_lambda.zip")
   role             = aws_iam_role.lambda_exec_role.arn
 
   environment {
