3ed63c300c537738397c7a4a050437ccc4753830
Ulyses Solon <ulyses.a.solon.jr@accenture.com>
2025-06-16 08:16:59 +0800

    Test main.tf with no profile = "default"

--- infra/main.tf
+++ infra/main.tf
@@ -1,6 +1,6 @@
 provider "aws" {
   region  = "us-east-1"
-  profile = "default"  # use the same name you used in `aws configure`
+  # profile = "default"  # use the same name you used in `aws configure`
 }
 
 terraform {
