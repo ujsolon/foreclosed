af5967a2b806f63e708cbbe8f93a020512a46aa9
Ulyses Solon <ulyses.a.solon.jr@accenture.com>
2025-06-17 06:39:51 +0800

    Create IAM access policy on dynamo resource

--- infra/dynamo.tf
+++ infra/dynamo.tf
@@ -14,3 +14,27 @@ resource "aws_dynamodb_table" "properties" {
     Project     = "foreclosed"
   }
 }
+
+resource "aws_iam_policy" "lambda_dynamodb_access_policy" {
+  name = "lambda_dynamodb_access_policy"
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Action = [
+          "dynamodb:GetItem",
+          "dynamodb:PutItem",
+          "dynamodb:UpdateItem"
+        ],
+        Resource = "arn:aws:dynamodb:us-east-1:*:table/properties"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "attach_dynamodb_access_policy" {
+  role       = aws_iam_role.lambda_exec_role.name
+  policy_arn = aws_iam_policy.lambda_dynamodb_access_policy.arn
+}
